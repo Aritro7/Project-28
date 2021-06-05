c8fa400204ebca78c24c363c93a1152ee27d7a0a
Aritro7 <81419739+Aritro7@users.noreply.github.com>
2021-06-05 23:36:50 +0600

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,26 @@
+class Launcher {
+
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.01,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.Launcher = Constraint.create(options);
+        World.add(world, this.Launcher);
+    }
+
+    fly(){
+        this.Launcher.bodyA=null;
+    }
+
+    Launch(bodyA){
+        this.launcherObj.bodyA=bodyA;
+    }
+
+    display(){
+
+    }
+}
